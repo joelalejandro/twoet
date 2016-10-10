1cf3f4a0e437d602329f0f4e1f6abee06359a0f3
Joel A. Villarreal Bertoldi <joel.a.villarreal@gmail.com>
2016-10-10 11:49:50 -0300

    added twitter tags, extracted fb/tw metadata methods

--- server/index.js
+++ server/index.js
@@ -9,16 +9,29 @@ import Config from 'config';
 const app = express();
 const args = minimist(process.argv.slice(2));
 
-const readTwoem = (poem, res) => {
-  res.write('<!doctype html>');
-  res.write('<html lang="es">');
-  res.write('<head>');
-  res.write(`<title>${poem.title}</title>`);
+const addFacebookTags = (poem, res) => {
   res.write(`<meta property="og:url" content="http://twoemme.com/${poem.id}" />`);
   res.write('<meta property="og:type" content="article" />');
   res.write(`<meta property="og:title" content="${poem.title}" />`);
   res.write(`<meta property="og:description" content="Un poema colaborativo escrito por ${poem.authors.map(author => { return '@' + author.alias; })}" />`);
+};
+
+const addTwitterTags = (poem, res) => {
+  const twitter = Config.get('twitter');
+  res.write(`<meta name="twitter:card" content="summary" />`);
+  res.write(`<meta name="twitter:site" content="${twitter.card_username}" />`);
+  res.write(`<meta name="twitter:title" content="${poem.title}" />`);
+  res.write(`<meta name="twitter:description" content="Un poema colaborativo escrito por ${poem.authors.map(author => { return '@' + author.alias; })}" />`);
+};
+
+const readTwoem = (poem, res) => {
+  res.write('<!doctype html>');
+  res.write('<html lang="es">');
+  res.write('<head>');
   res.write('<meta charset="UTF-8">');
+  res.write(`<title>${poem.title}</title>`);
+  addFacebookTags(poem, res);
+  addTwitterTags(poem, res);
   res.write('<style>body{font-family:monospace;font-size:2em;text-align:center}input{font-family:monospace;font-size:1em}.json-markup{line-height:1.2em;font-size:1em;font-family:monospace;white-space:pre-line}.json-markup-key{font-weight:700}.json-markup-bool{color:#b22222}.json-markup-string{color:green}.json-markup-null{color:gray}.json-markup-number{color:#00f}</style>');
   res.write('</head>');
   res.write('<body>');
