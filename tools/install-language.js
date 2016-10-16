392f9bfeef3a2e18492a147b4dd4f78d3a6d0351
Joel A. Villarreal Bertoldi <joel.a.villarreal@gmail.com>
2016-10-15 23:17:09 -0300

    refactored language setup, now installs rhymer and syllabler

--- tools/install-language.js
+++ tools/install-language.js
@@ -2,30 +2,29 @@ import Config from 'config';
 import { exec } from 'child_process';
 
 const language = Config.get('twoet.detournement_context.language');
-const plugin = Config.get(`twoet.rhyming_plugins.${language}`);
-
 console.log(`=== Installing language: ${language}`);
 
-console.log(`=== Begin installing NPM package => ${plugin.package}`);
+['rhyming', 'syllable'].forEach((pluginType) => {
+  const plugin = Config.get(`twoet.${pluginType}_plugins.${language}`);
 
-exec(`npm install ${plugin.package}`, (error, stdout, stderr) => {
-  if (error !== null) {
-    console.log(stderr);
-    return;
-  } else {
-    console.log('=== Finished installing NPM package');
-    if (plugin.post_install && plugin.post_install !== '') {
-      console.log('=== Running post_install commands');
-      exec(plugin.post_install, (error, stdout, stderr) => {
-        if (error !== null) {
-          console.log('=== post_install commands failed, installation may be incomplete');
-        } else {
-          console.log('=== Finished running post_install commands');
-          console.log('=== Finished language installation');
-        }
-      });
+  console.log(`=== Begin installing ${pluginType} plugin => ${plugin.package}`);
+
+  exec(`npm install ${plugin.package}`, (error, stdout, stderr) => {
+    if (error !== null) {
+      console.log(stderr);
+      return;
     } else {
-      console.log('=== Finished language installation');
+      console.log(`=== Finished installing ${plugin.package}`);
+      if (plugin.post_install && plugin.post_install !== '') {
+        console.log('=== Running post_install commands');
+        exec(plugin.post_install, (error, stdout, stderr) => {
+          if (error !== null) {
+            console.log('=== post_install commands failed, installation may be incomplete');
+          } else {
+            console.log('=== Finished running post_install commands');
+          }
+        });
+      }
     }
-  }
+  });
 });
